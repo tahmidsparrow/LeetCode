2cc8dc946204a03fa8e8c8595f4623ab49c89686
Md Tahmidul Abedin <aunik43@gmail.com>
2024-05-27 00:26:31 +0600

    Added problem and solution of #35
    Added solution of the problem #35 using C++ in 11ms.

---
+++ 35. Search Insert Position/searchInsert.cpp
@@ -0,0 +1,22 @@
+// Source : https://leetcode.com/problems/search-insert-position
+// Date   : Jan 11, 2022
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0;
+        int end =nums.size()-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid; 
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else{
+             start = mid+1;
+         }
+        }
+        return start;
+    }
+};
